153845a72a535f7e38e75ba7a07d73ecd50d2bcf
Olivia Arscott <olivia.arscott@mitel.com>
2021-06-18 07:36:33 -0400

    Assignment3

--- Angular/basics-assignment-3-start/src/app/app.component.ts
+++ Angular/basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .textColour {
+      color: white
+    }
+  `]
 })
 export class AppComponent {
+  showSecret = false;
+  count = 0;
+  clickLog = [];
+
+  onClick() {
+    this.showSecret =! this.showSecret;
+    this.count += 1;
+    this.clickLog.push({'count': this.count, 'event': 'click'});
+  }
+
+  getBackgroundColour(number: number) {
+    if ( number > 5 ) {
+      return 'blue'
+    } 
+  }
 }
